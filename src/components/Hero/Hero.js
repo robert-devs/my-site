98aec74609fb265ba6dea40fc41b0fdd091eaeec
robert-devs <mainrobert04@gmail.com>
2023-03-15 15:15:37 +0300

    adding the header and the hero sections

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from 'react'
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents'
+import Button from '../../styles/GlobalComponents/Button'
+import { LeftSection } from './HeroStyles'
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br /> My Personal portfolio
+      </SectionTitle>
+      <SectionText>
+        I offer solution on the various design, content-creating and website
+        development to both individuals and also to firm who may need certain
+        assistance in their website and softwares
+      </SectionText>
+      <Button onClick={() => (window.location = 'http://google.com')}>
+        {' '}
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero

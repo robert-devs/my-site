e7d6b6fc304921e1db56a4ba8d96102680331786
robert-devs <mainrobert04@gmail.com>
2023-03-16 10:52:21 +0300

    adding the timeline section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => (
     </Div1>
     <Div2>
       <li>
-        <Link href="#project">
+        <Link href="#projects">
           <NavLink>Projects</NavLink>
         </Link>
       </li>

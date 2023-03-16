7fa6eb9c778228cc58e259ebd4e84c2ba8a8471d
robert-devs <mainrobert04@gmail.com>
2023-03-16 11:34:17 +0300

    adding the footert section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from 'react'
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from '../Header/HeaderStyles'
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  );
-};
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle> Call</LinkTitle>
+          <LinkItem href="tel:0700812847"> 0700812847</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle> Email</LinkTitle>
+          <LinkItem href="mailto:mainrobert04@gmail.com">
+            {' '}
+            mainrobert04@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan> Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="http://github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="http://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="http://Instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  )
+}
 
-export default Footer;
+export default Footer
